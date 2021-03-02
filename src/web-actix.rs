d4bdbd0aec940241dcbfd5896083e83e5912de72
Bill Barekas <bb@wappier.com>
2021-03-02 20:19:27 +0200

    Add workers()

--- src/web-actix.rs
+++ src/web-actix.rs
@@ -10,11 +10,22 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 /// * (Responder): object that has implements the Responder trait
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
+    println!("Received: {:?}", req);
     format!("Hello {}!", &name)
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    let test = String::from("test");
+    let test_function = || {
+        println!("{}", test);
+        return test + &String::from(" case")
+
+    };
+    let outcome: String = test_function();
+    println!("out: {}", outcome);
+
     HttpServer::new(|| {
         println!("function is firing");
         let app = App::new()
@@ -23,6 +34,7 @@ async fn main() -> std::io::Result<()> {
         return app
     })
         .bind("127.0.0.1:8000")?
+        .workers(3)
         .run()
         .await
 }
