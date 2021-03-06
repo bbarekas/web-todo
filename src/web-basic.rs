d0a6edffd2cf12824d7a7a959698e95fc45c27e8
Bill Barekas <bb@wappier.com>
2021-03-06 15:26:10 +0200

    Finalize chapter4

--- src/web-basic.rs
+++ src/web-basic.rs
@@ -7,18 +7,6 @@ mod json_serialization;
 mod views;
 mod processes;
 
-/*
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let app = App::new().configure(views::views_factory);
-        return app;
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-*/
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
